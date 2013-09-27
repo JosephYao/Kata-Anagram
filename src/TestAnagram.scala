0ff13dde53307a148e095e8bf6edf74344eb93c5
josephyao <dermott@126.com>
2013-09-27 23:11:02 +0800

    one word list and another word that double this word should return something like "ab ab"

--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -8,11 +8,12 @@ class TestAnagram extends JUnitSuite {
     assertAnagramsEquals(List(), List(), "anyword")
   }
   
-  @Test def one_character_word_list_and_double_this_character_word {
+  @Test def one_word_list_and_another_word_that_double_this_word {
     assertAnagramsEquals(List("a a"), List("a"), "aa")
     assertAnagramsEquals(List("b b"), List("b"), "bb")
+    assertAnagramsEquals(List("ab ab"), List("ab"), "abab")
   }
-  
+
   private def assertAnagramsEquals(
       expectedAnagrams: List[String], wordList: List[String], word: String) = {
     val generator = new Anagram(wordList)
