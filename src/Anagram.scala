190489c4077972415a8dee3e52c6816e951b9d34
josephyao <dermott@126.com>
2013-10-07 21:41:25 +0800

    one word list and input is double of this word

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,4 +1,9 @@
 class Anagram(val wordList: List[String]) {
 
-  def generate(input: String) = List()
+  def generate(input: String) = wordList match {
+    case (head :: tail) if head + head == input =>
+      List(head + " " + head)
+    case _ =>
+      List()
+  }
 }
