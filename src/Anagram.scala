cc25712d9fb92b2ba4e15527d5b0310b58f172cd
josephyao <dermott@126.com>
2013-10-27 22:55:40 +0800

    two words list and input is double of any word except the first

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,8 +1,11 @@
 class Anagram (val wordList: List[String]) {
   
-  def generate(input: String): List[String] = wordList match {
+  def generate(input: String, wordList: List[String] = wordList): List[String] = 
+    wordList match {
     case (head :: tail) if head + head == input =>
       List(head + " " + head)
+    case (head :: tail) =>
+      generate(input, tail)
     case _ =>
       List()
   }
--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -15,6 +15,11 @@ class TestAnagram extends JUnitSuite {
     assertAnagramsEquals(List("ab ab"), List("ab"), "abab")
   }
   
+  @Test def two_words_list_and_input_is_double_of_any_word_except_the_first {
+    assertAnagramsEquals(List("b b"), List("a", "b"), "bb")
+    assertAnagramsEquals(List("c c"), List("a", "b", "c"), "cc")
+  }
+  
   private def assertAnagramsEquals(
       expectedAnagrams: List[String], wordList: List[String], input: String) = {
     val generator = new Anagram(wordList)
