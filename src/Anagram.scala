398f2f21fcc4b11d6d990cb077792c644d6b3c4e
josephyao <dermott@126.com>
2013-11-01 22:11:35 +0800

    more than one word list and input is double of non first word

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,10 +1,14 @@
 class Anagram(val wordList: List[String]){
   
-  def generate(input: String) = wordList match {
-    case (head :: tail) if (head + head == input) 
-      => List(head + " " + head)
-    case _ 
-      => List() 
+  def generate(input: String):List[String] = {
+    if (wordList.isEmpty)
+      return List()
+    
+    for (word <- wordList)
+	    if (word + word == input)
+	      return List(word +  " " + word)
+    
+    return List() 
   }
 
 }
