833abb71caa94452f8f31e75e1732281738b20d0
josephyao <dermott@126.com>
2013-11-01 22:00:45 +0800

    one word list and input is double of this word

--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -5,10 +5,21 @@ import org.junit.Assert.assertEquals
 class TestAnagram extends JUnitSuite {
   
   @Test def empty_word_list_and_input_is_any_word {
-	val generator = new Anagram(List())
-	val anagrams = generator.generate("anyword")
-	assertEquals(List(), anagrams)
+    assertAnagramsEqual(List(), List(), "anyword")
   }
 
+  @Test def one_word_list_and_input_is_double_of_this_word {
+    assertAnagramsEqual(List("a a"), List("a"), "aa")
+    assertAnagramsEqual(List("b b"), List("b"), "bb")
+    assertAnagramsEqual(List(), List("a"), "bb")
+    assertAnagramsEqual(List("ab ab"), List("ab"), "abab")
+  }
+  
+  private def assertAnagramsEqual(expectedAnagrams: List[String], 
+      wordList: List[String], input: String) = {
+    val generator = new Anagram(wordList)
+    val anagrams = generator.generate(input)
+    assertEquals(expectedAnagrams, anagrams)
+  }
 
 }
