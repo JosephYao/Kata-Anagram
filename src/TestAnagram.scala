f206a935b043083039449c2dd7db6e7af8a64e3d
josephyao <dermott@126.com>
2014-05-03 22:12:09 +0800

    migrate to IntelliJ

--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -1,7 +1,7 @@
 import org.scalatest.junit.JUnitSuite
 import org.junit.Test
 import org.junit.Assert.assertEquals
-
+ 
 class TestAnagram extends JUnitSuite {
 
   @Test def empty_word_list_and_input_is_any_word {
