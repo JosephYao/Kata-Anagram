782c4d4c45e0081aa9520b615df14927353904fa
josephyao <dermott@126.com>
2014-05-21 23:11:40 +0800

    Test: one character word list and double of this character word TPP: unconditional -> if, the new execution path is a nil -> constant

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,5 +1,11 @@
 class Anagram (wordList: List[String]) {
 
-  def generate(input: String) = List()
+  def generate(input: String): List[String] =
+  {
+    if (wordList.length == 0)
+      return List()
+
+    return List("a a")
+  }
 
 }
