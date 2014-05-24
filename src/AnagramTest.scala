56121100a5cb1fc613fa85b9ab047f2aad0706de
josephyao <dermott@126.com>
2014-05-24 22:50:35 +0800

    Test: one character word list and input is double of this character TPP: unconditional -> if, the new execution path is nil -> constant

--- src/AnagramTest.scala
+++ src/AnagramTest.scala
@@ -4,9 +4,17 @@ import org.junit.Assert.assertEquals
 class AnagramTest {
 
   @Test def empty_word_list_and_input_is_any_word {
-      val generator = new Anagram(List())
-      val anagrams = generator.generate("anyWord")
-      assertEquals(List(), anagrams)
+    assertAnagramEquals(List(), List(), "anyWord")
+  }
+
+  @Test def one_character_word_list_and_input_is_double_of_this_character {
+    assertAnagramEquals(List("a a"), List("a"), "aa")
+  }
+
+  private def assertAnagramEquals(expectedAnagrams: List[String], wordList: List[String], input: String) {
+    val generator = new Anagram(wordList)
+    val anagrams = generator.generate(input)
+    assertEquals(expectedAnagrams, anagrams)
   }
 
 }
