6c1b8be52d2a56d46bfe97fc9cb8d714199ee272
josephyao <dermott@126.com>
2014-06-10 22:34:47 +0800

    Test: two word list and input has no anagram

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,16 +1,16 @@
 class Anagram(wordList: List[String]) {
 
   def generate(input: String): List[String] = {
-    if (wordList.length == 2)
+    if (wordList.length == 2 && isAnagram(input, wordList))
       return List(wordList.mkString(" "))
 
-    if (!wordList.isEmpty && isAnagram(input))
+    if (!wordList.isEmpty && isAnagram(input, wordList ::: wordList))
       return List(wordList.head + " " + wordList.head)
 
     return List()
   }
 
-  def isAnagram(input: String) =
-    (wordList.head + wordList.head).sorted == input.sorted
+  def isAnagram(input: String, wordCombination: List[String]) =
+    wordCombination.mkString.sorted == input.sorted
 
 }
--- src/AnagramTest.scala
+++ src/AnagramTest.scala
@@ -22,6 +22,11 @@ class AnagramTest {
 
   @Test def two_word_list_and_input_is_the_combination_of_two_words {
     assertAnagramEquals(List("a b"), List("a", "b"), "ab")
+    assertAnagramEquals(List("ac bd"), List("ac", "bd"), "abcd")
+  }
+
+  @Test def two_word_list_and_input_has_no_anagram {
+    assertAnagramEquals(List(), List("a", "b"), "ac")
   }
 
   private def assertAnagramEquals(expectedAnagrams: List[String], wordList: List[String], input: String) {
