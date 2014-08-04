bd4ad1534b47b424770118f94d27c2fa8b3061cc
josephyao <dermott@126.com>
2014-08-03 21:12:17 -0700

    one character word list and input is double of this character TPP: unconditional -> if, the new execution path is nil -> constant

--- src/AnagramTest.scala
+++ src/AnagramTest.scala
@@ -5,9 +5,16 @@ import org.junit.Assert.assertEquals
 class AnagramTest extends JUnitSuite{
 
   @Test def empty_word_list_and_input_is_any_word {
-    val generator = new Anagram(List())
-    val anagrams = generator.generate("anyWord")
-    assertEquals(List(), anagrams)
+    assertAnagramsEquals(List(), "anyWord", List())
   }
 
+  @Test def one_character_word_list_and_input_is_double_of_this_character {
+    assertAnagramsEquals(List("a a"), "aa", List("a"))
+  }
+
+  def assertAnagramsEquals(expectedAnagrams: List[String], input: String, wordList: List[String]) {
+    val generator = new Anagram(wordList)
+    val anagrams = generator.generate(input)
+    assertEquals(expectedAnagrams, anagrams)
+  }
 }
