05926a8e49924cb42e456c0aae25c131370626cc
josephyao <dermott@126.com>
2014-08-10 22:03:56 +0800

    one character word list and input is double of this character TPP: unconditional -> if. This time, I took the "wordList.isEmpty" condition in the first place instead of using other possible condition like "input == "aa"". The reason why I chose it in the first place is that it's actually the condition for the first test case. It's not in the code just because no need to add if at that time. I prefer such if transformation since it introduced no new logic even a new if path is added

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,4 +1,9 @@
 class Anagram (wordList: List[String]) {
-  def generate(input: String) = List()
+  def generate(input: String): List[String] = {
+    if (wordList.isEmpty)
+      return List()
+
+    return List("a a")
+  }
 
 }
--- src/AnagramTest.scala
+++ src/AnagramTest.scala
@@ -5,9 +5,16 @@ import org.junit.Assert.assertEquals
 class AnagramTest extends JUnitSuite {
   
   @Test def empty_word_list_and_any_word_input {
-    val generator = new Anagram(List())
-    val anagrams = generator.generate("anyWord")
-    assertEquals(List(), anagrams)
+    assertAnagramsEqual(List(), "anyWord", List())
   }
 
+  @Test def one_character_word_list_and_input_is_double_of_this_character {
+    assertAnagramsEqual(List("a a"), "aa", List("a"))
+  }
+
+  def assertAnagramsEqual(expectedAnagrams: List[String], input: String, wordList: List[String]) {
+    val generator = new Anagram(wordList)
+    val anagrams = generator.generate(input)
+    assertEquals(expectedAnagrams, anagrams)
+  }
 }
