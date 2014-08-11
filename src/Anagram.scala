ca8c4c9603e4f1241df1ddcb4ef5645726e7f8e2
josephyao <dermott@126.com>
2014-08-11 22:17:37 +0800

    one word list and input has no anagram TPP: statement -> statements ( wordList.head + wordList.head == input is the new statement added)

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,9 +1,9 @@
 class Anagram (wordList: List[String]) {
   def generate(input: String): List[String] = {
-    if (wordList.isEmpty)
-      return List()
+    if (!wordList.isEmpty && wordList.head + wordList.head == input)
+      return List(wordList.head + " " + wordList.head)
 
-    return List(wordList.head + " " + wordList.head)
+    return List()
   }
 
 }
