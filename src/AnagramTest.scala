9c44f3054d46e01709f8fd3335fc731c1774a01b
josephyao <dermott@126.com>
2014-08-11 22:27:47 +0800

    one word list and input is double of all this word characters but different to spelling order of word TPP: expression -> function (sorted algorithm is added)

--- src/AnagramTest.scala
+++ src/AnagramTest.scala
@@ -18,6 +18,10 @@ class AnagramTest extends JUnitSuite {
     assertAnagramsEqual(List(), "aa", List("b"))
   }
 
+  @Test def one_word_list_and_input_is_double_of_all_this_word_characters_but_different_to_spelling_order_of_this_word {
+    assertAnagramsEqual(List("ab ab"), "abba", List("ab"))
+  }
+
   def assertAnagramsEqual(expectedAnagrams: List[String], input: String, wordList: List[String]) {
     val generator = new Anagram(wordList)
     val anagrams = generator.generate(input)
