f862fd839cb44cb568c0478dc5a327eb1534b47a
josephyao <dermott@126.com>
2014-10-27 20:46:15 +0800

    one character word list and input is double of this character TPP: unconditional -> if

--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -5,8 +5,15 @@ import org.scalatest.junit.JUnitSuite
 class TestAnagram extends JUnitSuite {
 
   @Test def empty_word_list_and_any_word {
-    val generator = new Anagram(List())
-    assertEquals(List(), generator.generate("anyWord"))
+    assertAnagramEqual(List(), "anyWord", List())
   }
 
+  @Test def one_character_word_list_and_input_is_double_of_this_character {
+    assertAnagramEqual(List("a a"), "aa", List("a"))
+  }
+
+  def assertAnagramEqual(expected: List[String], input: String, wordList: List[String]) {
+    val generator = new Anagram(wordList)
+    assertEquals(expected, generator.generate(input))
+  }
 }
