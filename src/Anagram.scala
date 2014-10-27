1e8e6519ef45c846a29b1e896feec7c4d044fa68
josephyao <dermott@126.com>
2014-10-27 22:19:14 +0800

    one word list and input has no anagram TPP: statement -> statements

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,9 +1,11 @@
 class Anagram (wordList: List[String]) {
   def generate(input: String): List[String] = {
-    if (wordList.isEmpty)
-      return List()
+    if (isAnagram(input))
+      return List(wordList.head + " " + wordList.head)
 
-    return List(wordList.head + " " + wordList.head)
+    return List()
   }
 
+  def isAnagram(input: String) =
+    !wordList.isEmpty && wordList.head + wordList.head == input
 }
--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -14,6 +14,10 @@ class TestAnagram extends JUnitSuite {
     assertAnagramEqual(List("ab ab"), "abab", List("ab"))
   }
 
+  @Test def one_word_list_and_input_has_no_anagram {
+    assertAnagramEqual(List(), "bb", List("a"))
+  }
+
   def assertAnagramEqual(expected: List[String], input: String, wordList: List[String]) {
     val generator = new Anagram(wordList)
     assertEquals(expected, generator.generate(input))
