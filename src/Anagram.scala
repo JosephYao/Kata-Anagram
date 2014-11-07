b1cb639f945c23b0076308cfb84ce51da87d235e
josephyao <dermott@126.com>
2014-11-07 11:16:33 +0800

    any word list and input only has anagram of any one word TPP: expression -> function, generalize to functional style

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,17 +1,9 @@
 class Anagram (wordList: List[String]) {
 
-  def generate(input: String): List[String] = {
-    if (!wordList.isEmpty && isAnagram(input, wordList.head))
-      return output(wordList.head)
+  def generate(input: String) =
+    wordList.filter(isAnagram(input, _)).map(output)
 
-    if (wordList.length == 2 && isAnagram(input, wordList(1)))
-      return output(wordList(1))
-
-    return List()
-  }
-
-  def output(anagram: String) =
-    List(anagram + " " + anagram)
+  def output(anagram: String) = anagram + " " + anagram
 
   def isAnagram(input: String, candidate: String) =
     (candidate + candidate).sorted == input.sorted
