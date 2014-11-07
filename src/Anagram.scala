8e080a45f3305f34ae98d79ce5f41b49dd54e397
josephyao <dermott@126.com>
2014-11-07 11:02:36 +0800

    two word list and input only has anagram of second word TPP: unconditional -> if

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,12 +1,18 @@
 class Anagram (wordList: List[String]) {
 
   def generate(input: String): List[String] = {
-    if (isAnagram(input))
-      return List(wordList.head + " " + wordList.head)
+    if (!wordList.isEmpty && isAnagram(input, wordList.head))
+      return output(wordList.head)
+
+    if (wordList.length == 2 && isAnagram(input, wordList(1)))
+      return output(wordList(1))
 
     return List()
   }
 
-  def isAnagram(input: String) =
-    !wordList.isEmpty && (wordList.head + wordList.head).sorted == input.sorted
+  def output(anagram: String) =
+    List(anagram + " " + anagram)
+
+  def isAnagram(input: String, candidate: String) =
+    (candidate + candidate).sorted == input.sorted
 }
--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -21,6 +21,10 @@ class TestAnagram extends JUnitSuite {
   @Test def one_word_list_and_input_has_double_of_characters_with_different_order {
     assertAnagrams(List("ab ab"), "abba", List("ab"))
   }
+  
+  @Test def two_word_list_and_input_only_has_anagram_of_second_word {
+    assertAnagrams(List("a a"), "aa", List("b", "a"))
+  }
 
   def assertAnagrams(expectedAnagrams: List[String], input: String, wordList: List[String]) {
     val generator = new Anagram(wordList)
