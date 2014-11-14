e5e785c40c9fffef871e76f04022412f889a6644
josephyao <dermott@126.com>
2014-11-14 19:39:53 +0800

    any word list and input only has anagram of non first word TPP: change from non-functional style to functional style

--- src/Anagram.scala
+++ src/Anagram.scala
@@ -1,17 +1,11 @@
 class Anagram (wordList: List[String]) {
 
   def generate(input: String): List[String] = {
-    def output(anagram: String) = List(anagram + " " + anagram)
+    def output(anagram: String) = anagram + " " + anagram
 
     def isAnagram(candidate: String) =
       (candidate + candidate).sorted == input.sorted
 
-    if (wordList.length == 2 && isAnagram(wordList(1)))
-      return output(wordList(1))
-
-    if (!wordList.isEmpty && isAnagram(wordList.head))
-      return output(wordList.head)
-
-    return List()
+    return wordList.find(isAnagram).toList.map(output)
   }
 }
--- src/TestAnagram.scala
+++ src/TestAnagram.scala
@@ -22,8 +22,10 @@ class TestAnagram extends JUnitSuite {
     assertAnagrams(List("ab ab"), "abba", List("ab"))
   }
 
-  @Test def two_word_list_and_input_only_has_anagram_of_second_word {
+  @Test def any_word_list_and_input_only_has_anagram_of_non_first_word {
     assertAnagrams(List("a a"), "aa", List("b", "a"))
+    assertAnagrams(List("a a"), "aa", List("c", "b", "a"))
+    assertAnagrams(List("a a"), "aa", List("c", "b", "a", "d"))
   }
 
   def assertAnagrams(expectedAnagrams: List[String], input: String, wordList: List[String]) {
